85eda2b2fc51c9f6c170115b15528ef30687360e
mvbnffc <mark.bernhofen@smithschool.ox.ac.uk>
2025-08-19 13:25:08 +0100

    flood protection cost implementation

---
+++ rules/analyze/adaptation_costs.smk
@@ -0,0 +1,26 @@
+"""
+Calculate adaptation costs at the admin level
+"""
+
+rule flood_protection_costs:
+    """
+    This rule calculate the cost of river flood protection using the length of the river to be protected 
+    as well as the delta in flood protection (relative to baseline protection levels)
+    """
+    input:
+        admin_areas="data/inputs/boundaries/{ISO3}/geobounds_{ISO3}.gpkg",
+        flopros="data/inputs/analysis/countries/{ISO3}/{ISO3}_flopros.tif",
+        urban="data/inputs/analysis/countries/{ISO3}/{ISO3}_urbanization.gpkg",
+        rivers="data/inputs/analysis/countries/{ISO3}/{ISO3}_river_network.gpkg",
+        gdppc="config/gdppc_data.csv"
+    output:
+        protection_cost="data/results/adaptation/costs/countries/{ISO3}/{ISO3}_adaptation-cost_fp_rp{RP}_duc{urban_class}_{ADMIN_SLUG}.gpkg"
+    wildcard_constraints:
+        urban_class="11|12|13|21|22|23|30",
+        ADMIN_SLUG="ADM0|ADM1|ADM2"
+    script:
+        "./flood_protection_costs.py"
+""" 
+Test with
+snakemake -c1 data/results/adaptation/costs/countries/RWA/RWA_adaptation-cost_fp_rp100_duc30_ADM2.gpkg
+"""
---
+++ rules/analyze/flood_protection_costs.py
@@ -0,0 +1,179 @@
+"""
+This script calculates the cost of river flood protection at the admin scale, using the 
+length of river to be protected in every urban area as well as the flood protection delta
+(relative to baseline protection)
+"""
+
+import logging
+
+import rasterio
+from rasterio.mask import mask
+from pyproj import Geod
+from shapely.geometry import LineString
+import pandas as pd
+import geopandas as gpd
+import numpy as np
+import shapely
+from tqdm import tqdm
+
+if __name__ == "__main__":
+
+    try:
+        admin_path: str = snakemake.input["admin_areas"]
+        flopros_path: str = snakemake.input["flopros"]
+        river_path: str = snakemake.input["rivers"]
+        urban_path: str = snakemake.input["urban"]
+        gdppc_path: str = snakemake.input["gdppc"]
+        output_path: str = snakemake.output["protection_cost"]
+        urban_class: str = snakemake.wildcards.urban_class
+        administrative_level: int = snakemake.wildcards.ADMIN_SLUG
+        RP: str = snakemake.wildcards.RP
+        country: str = snakemake.wildcards.ISO3
+    except NameError:
+        raise ValueError("Must be run via snakemake.")
+    
+logging.basicConfig(format="%(asctime)s %(process)d %(filename)s %(message)s", level=logging.INFO)
+
+# Update notation for GADM
+admin_level = int(administrative_level.replace("ADM", ""))
+
+logging.info(f"Calculating river flood protection costs for {country} at admin level {admin_level}.")
+logging.info(f"Minimum urban area to be protected is DUC{urban_class} to RP {RP}")
+
+# -------------------HELPER FUNCTIONS -----------------------------------
+
+def calculate_geod_length(line):
+    '''
+    Function to caluclate the geodetic length of a LineString
+    '''
+    geod = Geod(ellps="WGS84") # our data is in WGS84 projection
+    length = 0
+    if isinstance(line, LineString):
+        for i in range(len(line.coords)-1):
+            lon1, lat1 = line.coords[i]
+            lon2, lat2 = line.coords[i + 1]
+            _, _, distance = geod.inv(lon1, lat1, lon2, lat2)
+            length += distance
+
+    return length / 1000 # convert to km
+
+def zonal_mode_for_polygon(src, geom):
+    '''
+    Function for raster stats per polygon (will calculate mode - for FLOPROS)
+    '''
+    # Mask raster to polygon, crop to bbox for speed
+    out, _ = mask(src, [geom], crop=True, filled=True)
+    arr = out[0]
+
+    # Remove nodata and NaNs
+    nodata = src.nodata
+    if nodata is not None:
+        arr = arr[arr != nodata]
+    arr = arr[~np.isnan(arr)]
+
+    if arr.size == 0:
+        return np.nan
+
+    # Compute mode (works for int or float)
+    vals, counts = np.unique(arr, return_counts=True)
+    return float(vals[np.argmax(counts)])
+
+def calculate_protection_costs(geom, protection_level, country_cost_adjustment):
+    """
+    Calculate the cost of river flood protection.
+    This is done using the river length in the admin region, the baseline
+    level of protection and the new design protection level.
+    We use the standard Boulange et al unit cost per km of river to be protected.
+    We also adjust the unit costs per country based on the GDP pc relative to global average.
+    We set a lower and upper limit of unit construction costs as per the Boulandge et al 
+    supplementary.
+    
+    This function will return both the standard (globally consistent) cost of river flood protection
+    and the country_adjusted one.
+    """
+    max_unit_cost = 7.196 # million
+    standard_unit_cost = 2.399 # million
+    min_unit_cost = 0.7966 # million
+
+    river_length = geom['riv_len_km']
+    baseline_protection = geom['FLOPROS']
+
+    # Protection delta
+    delta_fp = np.max((protection_level-baseline_protection), 0)
+
+    # Standard adaptation cost
+    adaptation_cost = standard_unit_cost * river_length * np.log2(delta_fp)
+
+    # Adjusted adaptation cost
+    adjusted_unit_cost = country_cost_adjustment * standard_unit_cost
+    if max_unit_cost < adjusted_unit_cost:
+        adjusted_unit_cost = max_unit_cost
+    elif min_unit_cost > adjusted_unit_cost:
+        adjusted_unit_cost = min_unit_cost
+    
+    adj_adaptation_cost = adjusted_unit_cost * river_length * np.log2(delta_fp)
+
+    return adaptation_cost, adj_adaptation_cost
+
+# -----------------------------------------------------------
+
+logging.info(f"Reading level {administrative_level} admin boundaries")
+layer_name = f"ADM{admin_level}"
+admin_areas: gpd.GeoDataFrame = gpd.read_file(admin_path, layer=layer_name)
+area_unique_id_col = "shapeName"
+admin_areas = admin_areas[[area_unique_id_col, "geometry"]]
+logging.info(f"There are {len(admin_areas)} admin areas to analyze.")
+
+logging.info(f"Reading the river network data")
+rivers = gpd.read_file(river_path)
+
+logging.info("Reading urbanization data")
+urban = gpd.read_file(urban_path)
+
+logging.info("Calculating the flood protection per urban area")
+with rasterio.open(flopros_path) as src:
+    # Ensure geometries are in the raster CRS
+    urban_in_raster_crs = urban.to_crs(src.crs)
+    modes = [] # will be calculating most common (mode) FLOPROS value per region
+    for geom in tqdm(urban_in_raster_crs.geometry, desc="calculating urban FLOPROS"):
+        modes.append(zonal_mode_for_polygon(src, geom))
+urban['FLOPROS'] = modes
+
+logging.info("Calculating the river length per urban area")
+urban_areas = urban[urban['DEGURBA_L2'] >= int(urban_class)] # Filter only by urban classes we are interested in
+# Intersect rivers with the urbanization layer
+urban_rivers = gpd.overlay(rivers, urban_areas, how='intersection')
+urban_rivers['riv_len_km'] = urban_rivers['geometry'].apply(calculate_geod_length)
+
+logging.info("Calculating the protection cost per urban area")
+# Get the GDP adjustment
+gdp_df = pd.read_csv(gdppc_path)
+country_gdp = gdp_df.loc[gdp_df["ISO"].eq(country), "GDP_pc"].iloc[0]
+global_gdp  = gdp_df.loc[gdp_df["ISO"].eq("WORLD"), "GDP_pc"].iloc[0]
+gdp_adjustment = country_gdp / global_gdp
+# Calculate the protection cost for each urban area
+urban_rivers[['adaptation_cost', 'adj_adaptation_cost']] = (
+    urban_rivers.apply(
+        lambda r: pd.Series(
+            calculate_protection_costs(
+                r, protection_level=int(RP), country_cost_adjustment=gdp_adjustment
+        )
+    ), axis = 1 
+    )
+)
+
+logging.info("Sum the protection costs per Admin region")
+# Intersect urban river data with admin data
+intersected_rivers = gpd.overlay(urban_rivers, admin_areas, how='intersection')
+# Group by admin region and sum sugment lengths
+costs_per_admin = intersected_rivers.groupby('shapeName')['adaptation_cost'].sum().reset_index()
+adj_costs_per_admin = intersected_rivers.groupby('shapeName')['adj_adaptation_cost'].sum().reset_index()
+# Add total costs the the admin area dataframe
+admin_areas = admin_areas.merge(costs_per_admin, how='left', left_on='shapeName', right_on='shapeName')
+admin_areas = admin_areas.merge(adj_costs_per_admin, how='left', left_on='shapeName', right_on='shapeName')
+
+logging.info("Writing reults to GeoPackage.")
+results_gdf = gpd.GeoDataFrame(admin_areas, geometry="geometry")
+results_gdf.to_file(output_path, driver="GPKG")
+
+logging.info("Done.")
